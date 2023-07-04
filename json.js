d2457d192bac8db5d7b2b21e0a4964b205db9bc8
Anthovilla <9anthovilla7@gmail.com>
2023-07-04 16:58:56 +0200

    first commit

---
+++ json.js
@@ -0,0 +1,30 @@
+const textReference = document.getElementById('valorInput')
+const saveButton = document.getElementById('btnsave')
+const loadButton = document.getElementById('btnload')
+const deleteButton = document.getElementById('btndelete')
+
+const save = function () {
+    const textContent = textReference.value
+    localStorage.setItem('note-inLS', textContent)
+    alert('Contenuto salvato ')
+    document.getElementById('valorInput').value = ""
+}
+saveButton.addEventListener('click',save)
+
+const load =function () {
+    const cardMemo = localStorage.getItem('note-inLS') 
+    if(cardMemo) {
+        textReference.value = cardMemo
+    }else {
+        alert('nope memory')
+    }
+    document.getElementById('valorDILS').innerHTML = cardMemo
+}
+loadButton.addEventListener('click',load)
+
+const reset =function() {
+    textReference.value = ''
+    localStorage.removeItem('note-inLS')
+}
+
+deleteButton.addEventListener('click',reset)
